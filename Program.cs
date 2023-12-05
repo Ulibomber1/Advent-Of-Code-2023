a7f9e4682a3e7ba5bbc28a4a87de351085df3a03
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2023-12-04 22:21:45 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Advent_Of_Code_2023
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            Console.WriteLine("Welcome to the Advent of Code 2023 CLI!\n\n");
+            bool isQuitting = false;
+            while (!isQuitting)
+            {
+                command = Console.ReadLine();
+                switch (command)
+                {
+                    case "/help":
+                        Console.WriteLine("Help is currently in development.");
+                        break;
+                    case "/quit":
+                        Console.WriteLine("Goodbye!");
+                        isQuitting = true;
+                        break;
+                    default:
+                        Console.WriteLine("Unknown command " + command);
+                        break;
+                }
+            }
+        }
+    }
+}
